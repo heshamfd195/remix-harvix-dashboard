47a6dbb911c1982611f394eb8cf082ec30dbaf24
Hesham <hesham.fouad195@gmail.com>
2022-09-20 17:30:20 +0500

    mui added

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,32 +1,11 @@
+import { Typography } from "@mui/material";
+import React from "react";
+
+
 export default function Index() {
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
-    </div>
+    <React.Fragment>
+      <Typography variant="h1">Home</Typography>
+    </React.Fragment>
   );
 }
